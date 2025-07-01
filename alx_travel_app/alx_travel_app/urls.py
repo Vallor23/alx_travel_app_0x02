d8895180e4aa4994335f450943e077328b602020
Vallor23 <enosnyabinda2017@gmail.com>
2025-07-01 14:38:21 +0300

    configure URL for verifying payment

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ..listings.views import verify_payment, InitiatePaymentView
+from  listings.views import verify_payment, InitiatePaymentView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
